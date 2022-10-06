ab7136039b07e7aed714c1739ebd63d2bed8e146
ismailzainu <114725145+ismailzainu@users.noreply.github.com>
2022-10-06 10:54:32 +0530

    Add files via upload

---
+++ 4number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+    int a,b,c;
+    printf("enetr the two number to add\n");
+    scanf("%d%d",&a,&b);
+    c=a+b;
+    printf("sum of two number=%d\n",c);
+    return 0;
+}
---
+++ 5 float and double.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    float num1;
+    double num2;
+    printf("enter a number");
+    scanf("%f",&num1);
+    printf("enter another number");
+    scanf("%1f",&num2);
+    printf("num1=%f\n",num1);
+    printf("num2=%1f\n",num2);
+    return 0;
+    }
