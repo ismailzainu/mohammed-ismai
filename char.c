756e7ad296d26dfe62ce87e2e6654ddb5c800c86
ismailzainu <114725145+ismailzainu@users.noreply.github.com>
2022-10-06 10:39:39 +0530

    Add files via upload

---
+++ char.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    char chr;
+    printf("enter the character:");
+    scanf("%c",&chr);
+    printf("you entered%c",chr);
+    return 0;
+}
---
+++ integer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    int a;
+    printf("enter an integer\n");
+    scanf("%d",&a);
+    printf("integer that you have enter is %d\n",a);
+
+}
+
